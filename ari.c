4e2858848f37b768f09009590afa7afe3fdd4ab0
PrathibhaAmalladinne <84717502+PrathibhaAmalladinne@users.noreply.github.com>
2021-06-30 22:35:42 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,9 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-#include <cs50.h>
 #include <math.h>
 #include <ctype.h>
-
 string ari(string s){
 int letters=0,words=0,sentences=0,p=0;
    do{
@@ -16,13 +12,13 @@ int letters=0,words=0,sentences=0,p=0;
 	    { sentences++;}
           p++;
         }
-	  while(p<strlen(s)); 
+	  while((s[p]!='.') || (s[p+1]!='\0')); 
   
 sentences=sentences+1;
 int index = trunc ((4.71*((float)letters/words))+(0.5*((float) words/sentences))-21.43)+1;
  string var;
  if(index==1){var = "kindergarten";}
- else if(index==2){var = " First/Second Grade";}
+ else if(index==2){var = "First/Second Grade";}
  else if(index==3){var = "Third Grade";}
  else if(index==4){var = "Fourth Grade";}
  else if(index==5){var = "Fifth Grade";}
@@ -31,9 +27,10 @@ int index = trunc ((4.71*((float)letters/words))+(0.5*((float) words/sentences))
  else if(index==8){var = "Eighth Grade";}
  else if(index==9){var = "Ninth Grade";}
  else if(index==10){var = "Tenth Grade";}
- else if(index==11){var = "Eleventh Grade";}
+ else if(index==11){var ="Eleventh Grade";}
  else if(index==12){var = "Twelfth grade";}
  else if(index==13){var = "College student";}
  else if(index==14){var = "Professor";}
+ printf("%s",var);
  return var;
  }
