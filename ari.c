5b50ce5d82b99264108facf5af7a26b37be088e6
PrathibhaAmalladinne <84717502+PrathibhaAmalladinne@users.noreply.github.com>
2021-06-30 21:28:33 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,31 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 #include <cs50.h>
-#include <ctype.h>
 #include <math.h>
+#include <ctype.h>
 
-string s[14] = {"Kindergarten", "First/Second Grade", "Third Grade", "Fourth Grade", "Fifth Grade", "Sixth Grade", "Seventh Grade",
-    "Eighth Grade", "Ninth Grade", "Tenth Grade", "Eleventh Grade", "Twelfth Grade", "College student", "Professor"};
-
-string ari(string s) {
-    int p = strlen(s);
-    int characters = 0;
-    int words = 0;
-    int sentences = 0;
-    for (int i=0;i<p;i++) {
-        if (isalnum(s[i])) {
-            characters +=1;
+string ari(string s){
+int letters=0,words=0,sentences=0,p=0;
+   do{
+	  if(isalnum(s[p]))
+	    {letters++; }
+	   
+	  if(s[p]==' ')
+	    {words++; }
+	  if((s[p]=='.') || (s[p]=='!') || (s[p]=='?'))
+	    { sentences++;}
+          p++;
         }
-        else if (s[i] == ' ') {
-            words +=1;
-        }
-        else if (s[i] == '.' || s[i] == '?' || s[i] == '!') {
-            sentences +=1;
-        }
-    }
-    float value = ((4.71*((float)characters/words)) + (0.5*((float)words/sentences)) - 21.43);
-    int a = (int)value;
-    int value2 = a +1;
-    int index = value2 - 1;
-    return s[index];
-}
+	  while((s[p]!='.') || (s[p+1]!='\0')); 
+  
+sentences=sentences+1;
+ //printf("\n%d\n%d\n%d\n",letters,words,sentences); 
+int index = trunc ((4.71*((float)letters/words))+(0.5*((float) words/sentences))-21.43)+1;
+ //printf("%d",index);
+ if(index==1){printf("kindergarten");}
+ else if(index==2){printf(" First/Second Grade");}
+ else if(index==3){printf("Third Grade");}
+ else if(index==4){printf("Fourth Grade");}
+ else if(index==5){printf("Fifth Grade");}
+ else if(index==6){printf("Sixth Grade ");}
+ else if(index==7){printf("Seventh Grade");}
+ else if(index==8){printf("Eighth Grade");}
+ else if(index==9){printf("Ninth Grade");}
+ else if(index==10){printf("Tenth Grade");}
+ else if(index==11){printf("Eleventh Grade");}
+ else if(index==12){printf("Twelfth grade");}
+ else if(index==13){printf("College student");}
+ else if(index==14){printf("Professor");}
+ }
